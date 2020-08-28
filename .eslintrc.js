ea35537b10fadc26fd14797fbd0819455303fd77
Zackarias Montell <zackarias@montell.se>
2020-08-28 17:51:08 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    indent: ["error", 4],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
+  }
+};
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# notes
+# Notes
 
+Project was set up using https://www.electronjs.org/docs/tutorial/first-app.
+
+## Getting started
+
+1. `npm install`
+1.
