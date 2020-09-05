d1e58ed4ad774418df17d49ba8c136bbbaa3a4b6
Christoffer Olsson <christoffer.olsson@ericsson.com>
2020-09-05 12:54:15 +0200

    Reading metadata from all files and presenting in list

--- app/components/app.tsx
+++ app/components/app.tsx
@@ -15,6 +15,7 @@ import {
 import fs from "fs";
 import path from "path";
 import os from "os";
+import readline from "readline";
 import FolderIcon from "@material-ui/icons/Folder";
 import VerticalDisplaySection from "./layout/vertical-display-section";
 import MarkdownEditor from "./features/markdown-editor";
@@ -42,7 +43,6 @@ const useStyles = makeStyles(() =>
       height: "100%",
       overflowX: "hidden",
       overflowY: "hidden",
-      fontSize: 20,
       fontFamily: "Roboto"
     },
     item: {
@@ -58,10 +58,18 @@ export type Note = {
   fileName?: string;
 };
 
+type FileDescription = {
+  fileName: string;
+  title: string;
+  tags: string[];
+  created: number;
+  modified: number;
+};
+
 const App = (): JSX.Element => {
   const classes = useStyles();
   const [zenMode, setZenMode] = useState<boolean>(false);
-  const [fileList, setFileList] = useState<string[]>([]);
+  const [fileList, setFileList] = useState<FileDescription[]>([]);
   const [currentNote, setCurrentNote] = useState<Note>({ content: "#" });
 
   const homeDir = useMemo<string>(() => os.homedir(), []);
@@ -86,15 +94,77 @@ const App = (): JSX.Element => {
     [folderPath]
   );
 
-  const fetchFiles = useCallback((): void => {
+  const fetchFiles = useCallback(async (): Promise<void> => {
     if (!fs.existsSync(folderPath)) {
       fs.mkdirSync(folderPath);
     }
     const folderContent = fs.readdirSync(folderPath);
-    setFileList(folderContent);
+
+    const filesWithMetadata = await Promise.all(
+      folderContent.map(
+        async (fileName: string): Promise<FileDescription> =>
+          new Promise<FileDescription>((resolve) => {
+            let readRowCount = 0;
+            const rowsToRead = 5;
+
+            const readStream = fs.createReadStream(
+              path.normalize(`${folderPath}/${fileName}`)
+            );
+            const reader = readline.createInterface({
+              input: readStream
+            });
+            let title = "";
+            let tags: string[] = [];
+            let created: number;
+            let modified: number;
+
+            reader.on("line", (line: string) => {
+              readRowCount += 1;
+              // TODO: Prevent reader from reading entire file if possible
+              // console.log(
+              //   "reader.on(line) callback, read rows: ",
+              //   readRowCount
+              // );
+
+              if (line.startsWith("tags: ")) {
+                tags = line
+                  .replace("tags: [", "")
+                  .replaceAll("]", "")
+                  .split(",");
+              }
+              if (line.startsWith("title: ")) {
+                title = line.replace("title: ", "");
+              }
+              if (line.startsWith("created: ")) {
+                created = Date.parse(
+                  line.replace("created: ", "").replaceAll("'", "")
+                );
+              }
+              if (line.startsWith("modified: ")) {
+                modified = Date.parse(
+                  line.replace("modified: ", "").replaceAll("'", "")
+                );
+              }
+
+              if (readRowCount === rowsToRead) {
+                reader.close();
+                readStream.destroy();
+                resolve({
+                  fileName,
+                  title,
+                  tags,
+                  created,
+                  modified
+                });
+              }
+            });
+          })
+      )
+    );
+
+    setFileList(filesWithMetadata);
 
     if (folderContent.length) {
-      // open first file and read from it
       openMarkdownFile(folderContent[0]);
     }
   }, [folderPath, openMarkdownFile]);
@@ -115,20 +185,23 @@ const App = (): JSX.Element => {
   const memoizedFileList = useMemo<React.ReactNode>(
     () =>
       fileList.length ? (
-        fileList.map((fileName) => (
+        fileList.map((file) => (
           <ListItem
             button
-            selected={currentNote.fileName === fileName}
+            selected={currentNote.fileName === file.fileName}
             onClick={() => {
-              openMarkdownFile(fileName);
+              openMarkdownFile(file.fileName);
             }}
-            key={fileName}
+            key={file.fileName}
           >
-            {fileName}
+            <ListItemText
+              primary={file.title}
+              secondary={file.tags.join(" & ")}
+            />
           </ListItem>
         ))
       ) : (
-        <Typography>No files created</Typography>
+        <ListItem disabled>No notes created</ListItem>
       ),
     [currentNote.fileName, fileList, openMarkdownFile]
   );
